84da2056b20388d3804ad41425a6f52eb4d3b044
svetlana-solo <to_s@list.ru>
2021-06-04 21:58:28 +0700

    Исправляет порядок условий в функции

--- js/main.js
+++ js/main.js
@@ -17,30 +17,29 @@ const checkType = function (min, max) {
 };
 
 const getRandomInteger = function (min, max) {
-  if (min >= max) {
-    return 'Первое число должно быть меньше второго';
+  if (checkType(min, max)) {
+    return 'Введите число';
   }
   if (checkPeriod(min, max)) {
     return 'Число должно быть положительное';
   }
-  if (checkType(min, max)) {
-    return 'Введите число';
+  if (min >= max) {
+    return 'Первое число должно быть меньше второго';
   }
   return Math.floor(getRandomNumberInPeriod(min, max));
 };
-//не могу пока понять почему не работет проверка по типу и как ее сделать универсальной, чтобы она проверяла не только min, max, а любые заданные параметры.
 
 getRandomInteger(5, 9);
 
 const getRandomFloatNumber = function (min, max, decimalPlaces) {
-  if (min >= max) {
-    return 'Первое число должно быть меньше второго ';
+  if (checkType(min, max)) {
+    return 'Введите число';
   }
   if (checkPeriod(min, max)) {
     return 'Число должно быть положительное';
   }
-  if (checkType(min, max)) {
-    return 'Введите число';
+  if (min >= max) {
+    return 'Первое число должно быть меньше второго ';
   }
   if (decimalPlaces < 0) {
     return 'Укажите положительное количество цифр после запятой';
