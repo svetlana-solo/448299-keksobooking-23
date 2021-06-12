e6c2a1b8477a482bf8c04082c2d3c67caf8ada39
svetlana-solo <to_s@list.ru>
2021-06-13 02:11:35 +0700

    Вносит доработку по заданию

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
-function getRandomPositiveInteger(alfa, betta) {
-  const lower = Math.ceil(Math.min(Math.abs(alfa), Math.abs(betta)));
-  const upper = Math.floor(Math.max(Math.abs(alfa), Math.abs(betta)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-function getRandomPositiveFloat(alfa, betta, digits = 1) {
-  const lower = Math.min(Math.abs(alfa), Math.abs(betta));
-  const upper = Math.max(Math.abs(alfa), Math.abs(betta));
-  const result = Math.random() * (upper - lower) + lower;
-  return result.toFixed(digits);
-}
-
-const AVATAR_AUTHOR = 'img/avatars/user' + getRandomPositiveInteger(1, 10) + '.png';
-
-const TYPE_LODGING = [
+const TYPES = [
   'palace',
   'flat',
   'house',
@@ -22,19 +6,13 @@ const TYPE_LODGING = [
   'hotel',
 ];
 
-const CHEK_IN = [
+const TIMES = [
   '12:00',
   '13:00',
   '14:00',
 ];
 
-const CHEK_OUT = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-
-const FEATURES_LODGING = [
+const FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -43,50 +21,112 @@ const FEATURES_LODGING = [
   'conditioner',
 ];
 
-const DESCRIPTION_LODGING = [
+const DESCRIPTIONS = [
   'уютное',
   'светлое',
   'просторное',
 ];
 
-const PHOTOS_LODGING = [
+const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-const LOCATION_LAT = getRandomPositiveFloat(35.65000, 35.70000, 5);
-const LOCATION_LNG = getRandomPositiveFloat(139.70000, 139.80000, 5);
-
-const location = {
-  lat: LOCATION_LAT,
-  lng: LOCATION_LNG,
+const getRandomNumberInPeriod = function (min, max) {
+  return min + Math.random() * (max + 1 - min);
 };
 
-const getRandomArrayElement = (elements) => {
-  return elements[_.getRandomPositiveInteger(0, elements.length - 1)];
+const checkPeriod = function (min, max) {
+  if (min < 0 || max < 0) {
+    return true;
+  }
+  return false;
 };
 
-const createOffer = () => {
+const checkType = function (min, max) {
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    return true;
+  }
+  return false;
+};
+
+const getRandomPositiveInteger = function (min, max) {
+  if (checkType(min, max)) {
+    return 'Введите число';
+  }
+  if (checkPeriod(min, max)) {
+    return 'Число должно быть положительное';
+  }
+  if (min >= max) {
+    return 'Первое число должно быть меньше второго';
+  }
+  return Math.floor(getRandomNumberInPeriod(min, max));
+};
+
+const getRandomPositiveFloat = function (min, max, decimalPlaces) {
+  if (checkType(min, max) || checkType(decimalPlaces)) {
+    return 'Введите число';
+  }
+  if (checkPeriod(min, max)) {
+    return 'Число должно быть положительное';
+  }
+  if (min >= max) {
+    return 'Первое число должно быть меньше второго ';
+  }
+
+  if (decimalPlaces < 0) {
+    return 'Укажите положительное количество цифр после запятой';
+  }
+  return +(getRandomNumberInPeriod(min, max).toFixed(decimalPlaces));
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+const createOffer = (index) => {
+  const minLat = 35.65000;
+  const maxLat = 35.70000;
+  const minLng = 139.70000;
+  const maxLng = 139.80000;
+  const decimalPlaces = 5;
+  const lat = getRandomPositiveFloat(minLat, maxLat, decimalPlaces);
+  const lng = getRandomPositiveFloat(minLng, maxLng, decimalPlaces);
+  const randomMaxFeature = getRandomPositiveInteger(1, FEATURES.length - 1);
+  const randomMaxPhoto = getRandomPositiveInteger(1, PHOTOS.length - 1);
+  const min = 0;
+  const minPosition = 1;
+  const maxPrice = 100000;
+  const maxRoom = 4;
+  const maxGuest = 3;
+  //Если у меня эти переменные используются только в этой функцие - могу я объявить их внутри и не выносить на самый верх?
   return {
-    title: 'Заголовок',
-    address: [location.lat, location.lng],
-    price: getRandomPositiveInteger(0, 1000000),
-    type: getRandomArrayElement(TYPE_LODGING),
-    rooms: getRandomPositiveInteger(1, 4),
-    guests: getRandomPositiveInteger(1, 4),
-    checkin: getRandomArrayElement(CHEK_IN),
-    checkout: getRandomArrayElement(CHEK_OUT),
-    features: getRandomArrayElement(FEATURES_LODGING),
-    description: getRandomArrayElement(DESCRIPTION_LODGING),
-    photos: getRandomArrayElement(PHOTOS_LODGING),
-    location: [LOCATION_LAT, LOCATION_LNG],
+    title: `Заголовок${index}`,
+    address: `${lat}, ${lng}`,
+    price: getRandomPositiveInteger(min, maxPrice),
+    type: getRandomArrayElement(TYPES),
+    rooms: getRandomPositiveInteger(minPosition, maxRoom),
+    guests: getRandomPositiveInteger(minPosition, maxGuest),
+    checkin: getRandomArrayElement(TIMES),
+    checkout: getRandomArrayElement(TIMES),
+    features: FEATURES.slice(min, randomMaxFeature),
+    description: getRandomArrayElement(DESCRIPTIONS),
+    photos: PHOTOS.slice(min, randomMaxPhoto),
+    location: {
+      lat,
+      lng,
+    },
   };
 };
 
-const createAuthor = () => {
+const ads = new Array(10).fill('').map((currentValue, index) => {
+  const correctedIndex = index + 1;
+  const userNumber = correctedIndex.toString().padStart(2, '0');
   return {
-    avatar: getRandomArrayElement(AVATAR_AUTHOR),
-    offer: createOffer(),
+    author: {
+      avatar: `img/avatars/user${userNumber}.png`,
+    },
+    offer: createOffer(correctedIndex),
   };
-};
+});
+
+console.log(ads);
