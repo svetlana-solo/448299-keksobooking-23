e545486a6099bacc4d48012445550c956cb873e1
svetlana-solo <to_s@list.ru>
2021-06-13 15:47:36 +0700

    вносит исправления в названия констант

--- js/main.js
+++ js/main.js
@@ -34,17 +34,17 @@ const PHOTOS = [
 ];
 
 const QUANTITY = 10;
-const MINLAT = 35.65000;
-const MAXLAT = 35.70000;
-const MINLNG = 139.70000;
-const MAXLNG = 139.80000;
-const DECIMALPLACES = 5;
-const MINPRICE = 0;
-const MAXPRICE = 100000;
-const MINROOM = 1;
-const MAXROOM = 4;
-const MINGUEST = 1;
-const MAXGUEST = 3;
+const MIN_LAT = 35.65000;
+const MAX_LAT = 35.70000;
+const MIN_LNG = 139.70000;
+const MAX_LNG = 139.80000;
+const DECIMAL_PLACES = 5;
+const MIN_PRICE = 0;
+const MAX_PRICE = 100000;
+const MIN_ROOM = 1;
+const MAX_ROOM = 4;
+const MIN_GUEST = 1;
+const MAX_GUEST = 3;
 
 const getRandomNumberInPeriod = function (min, max) {
   return min + Math.random() * (max + 1 - min);
@@ -97,17 +97,17 @@ const getRandomPositiveFloat = function (min, max, decimalPlaces) {
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
 const createOffer = (index) => {
-  const lat = getRandomPositiveFloat(MINLAT, MAXLAT, DECIMALPLACES);
-  const lng = getRandomPositiveFloat(MINLNG, MAXLNG, DECIMALPLACES);
+  const lat = getRandomPositiveFloat(MIN_LAT, MAX_LAT, DECIMAL_PLACES);
+  const lng = getRandomPositiveFloat(MIN_LNG, MAX_LNG, DECIMAL_PLACES);
   const randomMaxFeature = getRandomPositiveInteger(1, FEATURES.length - 1);
   const randomMaxPhoto = getRandomPositiveInteger(1, PHOTOS.length - 1);
   return {
     title: `Заголовок${index}`,
     address: `${lat}, ${lng}`,
-    price: getRandomPositiveInteger(MINPRICE, MAXPRICE),
+    price: getRandomPositiveInteger(MIN_PRICE, MAX_PRICE),
     type: getRandomArrayElement(TYPES),
-    rooms: getRandomPositiveInteger(MINROOM, MAXROOM),
-    guests: getRandomPositiveInteger(MINGUEST, MAXGUEST),
+    rooms: getRandomPositiveInteger(MIN_ROOM, MAX_ROOM),
+    guests: getRandomPositiveInteger(MIN_GUEST, MAX_GUEST),
     checkin: getRandomArrayElement(TIMES),
     checkout: getRandomArrayElement(TIMES),
     features: FEATURES.slice(0, randomMaxFeature),
